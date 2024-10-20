78c16227101132ea49fe6b4485c0de25a6e588e9
axel <axel.chalvin@gmail.com>
2024-10-20 13:27:14 +0200

    comments : maj des commentaires

--- app.py
+++ app.py
@@ -14,11 +14,15 @@ cur = conn.cursor()
 #MAIN FLASK ROUTE RETURNS CONNECTION PAGE
 @app.route("/")
 def index():
+    # Renders the connection page where users can log in.
+
     return render_template("connection.html")
 
 ## REGISTRATION
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    # Handles user registration by inserting user data into the database and redirects to the connection page.
+
     if request.method == "GET":
         return render_template("register.html")
     else:
@@ -34,11 +38,11 @@ def register():
 ## CONNECTION
 @app.route("/auth", methods=['GET','POST'])
 def auth():
-    #GET FORM VALUES
+    # Authenticates users by checking their login credentials and, if valid, initializes their session and renders the welcome page.
+
     user=request.form['username']
     password=request.form['password']
 
-    #SQL QUERY GET USER INFO
     cur.execute("SELECT * FROM public.user WHERE login=%s", (user,))
     data = cur.fetchall()
     if data:
@@ -56,9 +60,11 @@ def auth():
     else:
         return render_template("connection.html", error="Nom d'utilisateur inconnu ou mot de passe incorrect")
 
-## SESSION DELETE
+
 @app.route("/deco", methods=['GET','POST'])
 def deco():
+    # Clears the user session, effectively logging out the user, and redirects to the connection page.
+
     session.clear()
     return redirect(url_for('index'))
 
@@ -67,6 +73,7 @@ def deco():
 
 @app.route("/cave", methods=['GET','POST'])
 def cave():
+    # Manages the creation and display of caves for the user; it retrieves and renders the user's caves after creating a new one if submitted.
 
     user_obj = User(session['user']['name'], session['user']['ftname'], session['user']['login'], None)
     if request.form.get('cave_name'):
@@ -76,9 +83,10 @@ def cave():
 
 @app.route("/shelf", methods=['GET','POST'])
 def shelf():
+    # Handles the creation of shelves associated with a cave and displays the user's caves and shelves.
+
     if not session:
         return redirect(url_for('index'))
-
     user_obj = User(session['user']['name'], session['user']['ftname'], session['user']['login'], None)
     if request.form.get('shelf_name'):
         Shelf.createShelf(cur, request.form['shelf_name'], request.form['cave_id'], request.form['shelf_capacity'])
@@ -87,6 +95,8 @@ def shelf():
 
 @app.route("/bottle", methods=['GET','POST'])
 def bottle():
+    # Manages the addition of bottles to shelves and displays the user's bottles and related information.
+
     user_obj = User(session['user']['name'], session['user']['ftname'], session['user']['login'], None)
     if request.form.get('domain'):
         Bottle.createBottle(cur, request.form.get('shelf_id'), request.form.get('domain'), request.form.get('name'), request.form.get('type'), request.form.get('year'), request.form.get('region'), request.form.get('tag_picture'), request.form.get('price'))
@@ -95,6 +105,8 @@ def bottle():
 
 @app.route("/rate", methods=['GET','POST'])
 def rate():
+    # Processes user ratings and comments for bottles and renders the updated rate page for the user.
+
     user_obj = User(session['user']['name'], session['user']['ftname'], session['user']['login'], None)
     if request.form.get('rate'):
         Bottle.rate(cur, request.form.get('idbottle'), request.form.get('rate'), request.form.get('comment'))
@@ -106,24 +118,24 @@ def rate():
 
 @app.route('/delete/cave/<int:cave_id>', methods=['DELETE'])
 def delete_cave(cave_id):
-    # supprimer la cave avec l'id cave_id
+    # Deletes a specified cave from the database and returns a success message as JSON.
+
     Cave.deleteCave(cur, cave_id)
     return jsonify({'message': f'Cave supprimée avec succès'}), 200
 
 @app.route('/delete/shelf/<int:shelf_id>', methods=['DELETE'])
 def delete_shelf(shelf_id):
-    # supprimer l'étagère avec l'id shelf_id
+    # Deletes a specified shelf from the database and returns a success message as JSON.
+
     Shelf.deleteShelf(cur, shelf_id)
     return jsonify({'message': f'Étagère supprimée avec succès'}), 200
 
 @app.route('/delete/bottle/<int:bottle_id>', methods=['DELETE'])
 def delete_bottle(bottle_id):
-    # supprimer la bouteille avec l'id bottle_id
+    # Deletes a specified bottle from the database and returns a success message as JSON.
+
     Bottle.deleteBottle(cur, bottle_id)
     return jsonify({'message': f'Bouteille supprimée avec succès'}), 200
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
